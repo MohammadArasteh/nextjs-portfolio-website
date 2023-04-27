f9aa69cb2437f872a8a3726f5ef032ccdb17e616
Mohammad Arasteh <arasteh@gmail.com>
2023-04-27 14:57:00 +0330

    use next/image component instead of html img tag

--- src/components/Projects/ProjectsStyles.ts
+++ src/components/Projects/ProjectsStyles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import Image from "next/image";
 
-export const Img = styled.img`
+export const Img = styled(Image)`
   width: 100%;
   height: 100%;
   object-fit: cover;
